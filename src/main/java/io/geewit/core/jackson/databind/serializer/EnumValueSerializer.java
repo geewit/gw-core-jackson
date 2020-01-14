8a5e4b537ca15392bd6712838e2e41f3ebdee810
geewit <hf@geewit.io>
2020-01-14 15:09:52 +0800

    init

---
+++ src/main/java/io/geewit/core/jackson/databind/serializer/EnumValueSerializer.java
@@ -0,0 +1,23 @@
+package io.geewit.core.jackson.databind.serializer;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import io.geewit.core.utils.lang.enums.Value;
+import org.springframework.boot.jackson.JsonComponent;
+
+import java.io.IOException;
+
+/**
+ * 枚举类型序列化
+ * @author geewit
+ */
+@SuppressWarnings({"unused"})
+@JsonComponent
+public class EnumValueSerializer extends JsonSerializer<Value> {
+    @Override
+    public void serialize(Value value, JsonGenerator jgen, SerializerProvider provider) throws IOException {
+        // put your desired money style here
+        jgen.writeString(String.valueOf(value.value()));
+    }
+}
