65dff901cd3506dd09a11968a2cae80f5bbcf9cd
geewit <hf@geewit.io>
2020-02-24 16:11:05 +0800

    init

--- src/main/java/io/geewit/core/jackson/databind/serializer/JsonPageSerializer.java
+++ src/main/java/io/geewit/core/jackson/databind/serializer/JsonPageSerializer.java
@@ -2,8 +2,9 @@ package io.geewit.core.jackson.databind.serializer;
 
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializerProvider;
-import io.geewit.web.utils.JsonUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jackson.JsonComponent;
 import org.springframework.data.domain.Page;
 
@@ -16,6 +17,9 @@ import java.io.IOException;
 @SuppressWarnings({"unused"})
 @JsonComponent
 public class JsonPageSerializer extends JsonSerializer<Page> {
+
+    @Autowired
+    private ObjectMapper mapper;
 
     @Override
     public void serialize(Page page, JsonGenerator jsonGen, SerializerProvider serializerProvider) throws IOException {
@@ -36,7 +40,7 @@ public class JsonPageSerializer extends JsonSerializer<Page> {
         jsonGen.writeFieldName("numberOfElements");
         jsonGen.writeNumber(page.getNumberOfElements());
         jsonGen.writeFieldName("content");
-        jsonGen.writeRawValue(JsonUtils.toJson(page.getContent(), serializerProvider.getActiveView()));
+        jsonGen.writeRawValue(mapper.writerWithView(serializerProvider.getActiveView()).writeValueAsString(page.getContent()));
         jsonGen.writeEndObject();
     }
 
