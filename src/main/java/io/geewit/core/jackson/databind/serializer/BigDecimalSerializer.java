8b34a7b76ba06bd7bae505e1c2f106498457ba11
geewit <hf@geewit.io>
2020-05-18 19:27:40 +0800

    init

--- src/main/java/io/geewit/core/jackson/databind/serializer/BigDecimalSerializer.java
+++ src/main/java/io/geewit/core/jackson/databind/serializer/BigDecimalSerializer.java
@@ -19,6 +19,6 @@ public class BigDecimalSerializer extends JsonSerializer<BigDecimal> {
     @Override
     public void serialize(BigDecimal value, JsonGenerator jgen, SerializerProvider provider) throws IOException {
         // put your desired money style here
-        jgen.writeString(value.setScale(4, BigDecimal.ROUND_HALF_UP).toString());
+        jgen.writeString(value.setScale(2, BigDecimal.ROUND_HALF_UP).toString());
     }
 }
--- src/main/java/io/geewit/core/jackson/databind/serializer/JsonPageSerializer.java
+++ src/main/java/io/geewit/core/jackson/databind/serializer/JsonPageSerializer.java
@@ -4,8 +4,12 @@ import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializerProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.jackson.JsonComponent;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.io.IOException;
 
@@ -14,6 +18,7 @@ import java.io.IOException;
  * @author geewit
  */
 @SuppressWarnings({"unused"})
+@EnableConfigurationProperties({SpringDataWebProperties.class})
 @JsonComponent
 public class JsonPageSerializer extends JsonSerializer<Page> {
 
@@ -23,12 +28,25 @@ public class JsonPageSerializer extends JsonSerializer<Page> {
         this.mapper = mapper;
     }
 
+    @Autowired(required = false)
+    protected SpringDataWebProperties springDataWebProperties;
+
     @Override
     public void serialize(Page page, JsonGenerator jsonGen, SerializerProvider serializerProvider) throws IOException {
         jsonGen.writeStartObject();
-        jsonGen.writeFieldName("size");
+        String sizeParameter = "size";
+        String pageParameter = "number";
+        if(springDataWebProperties != null) {
+            SpringDataWebProperties.Pageable pageable = springDataWebProperties.getPageable();
+            if(pageable != null) {
+                sizeParameter = pageable.getSizeParameter();
+                pageParameter = pageable.getPageParameter();
+            }
+        }
+        jsonGen.writeFieldName(sizeParameter);
         jsonGen.writeNumber(page.getSize());
-        jsonGen.writeFieldName("number");
+
+        jsonGen.writeFieldName(pageParameter);
         jsonGen.writeNumber(page.getNumber());
         jsonGen.writeFieldName("totalElements");
         jsonGen.writeNumber(page.getTotalElements());
